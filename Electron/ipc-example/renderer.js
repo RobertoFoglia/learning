e95463bad29ff7f387c732ef73900922963313d3
ROBERTO-FOGLIA-\Roberto Foglia <fogliaroberto87@gmail.com>
2018-06-05 12:28:40 +0200

    Electron - synchronous IPC messagging

--- Electron/ipc-example/renderer.js
+++ Electron/ipc-example/renderer.js
@@ -1,3 +1,20 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const ipc = require("electron").ipcRenderer;
+
+const syncMsgBtn = document.getElementById("sendSyncMsgBtn");
+
+const syncReply = document.getElementById("syncReply");
+
+syncMsgBtn.addEventListener("click", function() {
+    syncReply.innerHTML = "Waiting for response";
+    const reply = ipc.sendSync(
+        "synchronous-message",
+        "Mr. Watson, come here.",
+        "I AM A RENDER PROCESS"
+    );
+    console.log(reply);
+    const message = `Synchronous message reply: ${reply}`;
+    syncReply.innerHTML = message;
+});

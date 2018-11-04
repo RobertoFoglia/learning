bacb441f668fa365057f926fd123e410096f821f
ROBERTO-FOGLIA-\Roberto Foglia <fogliaroberto87@gmail.com>
2018-11-04 15:14:24 +0100

    Chapter 2 initialization

---
+++ Vaadin/Vaadin 7 UI By Example Beginner's Guide/Chapter 2 - Forms and input components/time-it/src/main/java/timeit/ui/TimeItUi.java
@@ -0,0 +1,39 @@
+package timeit.ui;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser window 
+ * (or tab) or some part of an HTML page where a Vaadin application is embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * overridden to add component to the user interface and initialize non-component functionality.
+ */
+@Theme("mytheme")
+public class TimeItUi extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+        // @@@ businees classes separations
+
+        final VerticalLayout layout = new VerticalLayout();
+        
+        setContent(layout);
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "TimeItServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = TimeItUi.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+}

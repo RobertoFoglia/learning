a962c5bdd577c6476e9649b7091c5b8a441f7510
Roberto Foglia <roberto.foglia.computer.scientist@gmail.com>
2019-06-03 23:31:30 +0200

    Changing the default Mockito settings

--- Mockito/Chapter 3/src/test/java/com/packt/mockito/advanced/SettingsTest.java
+++ Mockito/Chapter 3/src/test/java/com/packt/mockito/advanced/SettingsTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.*;
 
 public class SettingsTest {
 
+	/** @@@ Changing the default Mockito settings - Mockito.RETURNS_DEFAULT pag 90 */
 	@Test
 	public void when_default_settings() throws Exception {
 
@@ -17,6 +18,7 @@ public class SettingsTest {
 
 	}
 
+	/** @@@ Changing the default Mockito settings - Mockito.RETURNS_SMART_NULLS pag 90 */
 	@Test
 	public void when_changing_default_settings_to_return_smartNULLS(){
 
@@ -28,6 +30,7 @@ public class SettingsTest {
 
 	}
 
+	/** @@@ Changing the default Mockito settings - Mockito.RETURNS_MOCKS pag 90 */
 	@Test
 	public void when_changing_default_settings_to_return_mocks() {
 
@@ -39,14 +42,22 @@ public class SettingsTest {
 		assertNotNull(mockBar);
 		assertNotNull(mockBar.getTar());
 		assertNotNull(mockBar.getTar().getName());
+
+		// it doesn't work because the Mockito.RETURNS_MOCKS doesn't allow us to stub the method in hierarchies
+		// when(fooWithReturnsMocks.getBar().getTar().getName()).thenReturn("Deep Stub");
+		// to do this you must use Mockito.RETURNS_DEEP_STUBS
+
 		System.out.println("fooWithReturnsMocks.getBar()=" + mockBar);
 		System.out.println("fooWithReturnsMocks.getBar().getTar().getName()={" + mockBar.getTar().getName()+"}");
 
 	}
 
+	/** @@@ Changing the default Mockito settings - Mockito.RETURNS_DEEP_STUBS pag 91 */
 	@Test
 	public void when_returns_deep_stub() throws Exception {
 		Foo fooWithDeepStub = Mockito.mock(Foo.class, Mockito.RETURNS_DEEP_STUBS);
+		// Without Mockito.RETURNS_DEEP_STUBS, the getBar() returns null value
+		// the Mockito.RETURNS_DEEP_STUBS allows us to stub the method in hierarchies
 		when(fooWithDeepStub.getBar().getTar().getName()).thenReturn("Deep Stub");
 		// a deep stubbed mock is returned
 		System.out.println("fooWithDeepStub.getBar().getTar().getName()="+ fooWithDeepStub.getBar().getTar().getName());

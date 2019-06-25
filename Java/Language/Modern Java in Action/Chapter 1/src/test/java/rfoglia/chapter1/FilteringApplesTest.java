1fd03068e392cc82ceba198a1985ccf67c9c74ea
Roberto Foglia <roberto.foglia.computer.scientist@gmail.com>
2019-06-25 06:50:42 +0200

    Chapter 1

---
+++ Java/Language/Modern Java in Action/Chapter 1/src/test/java/rfoglia/chapter1/FilteringApplesTest.java
@@ -0,0 +1,42 @@
+package rfoglia.chapter1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import rfoglia.chapter1.Apple;
+import rfoglia.chapter1.FilteringApples;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FilteringApplesTest {
+    @Test
+    public void main() {
+        List<Apple> inventory = Arrays.asList(
+                new Apple(80, "green"),
+                new Apple(155, "green"),
+                new Apple(120, "red")
+        );
+
+        // [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
+        List<Apple> greenApples = FilteringApples.filterApples(inventory, FilteringApples::isGreenApple);
+        System.out.println(greenApples);
+
+        // [Apple{color='green', weight=155}]
+        List<Apple> heavyApples = FilteringApples.filterApples(inventory, FilteringApples::isHeavyApple);
+        System.out.println(heavyApples);
+
+        // [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
+        List<Apple> greenApples2 = FilteringApples.filterApples(inventory, (a) -> "green".equals(a.getColor()));
+        System.out.println(greenApples2);
+
+        // [Apple{color='green', weight=155}]
+        List<Apple> heavyApples2 = FilteringApples.filterApples(inventory, a -> a.getWeight() > 150);
+        System.out.println(heavyApples2);
+
+        // []
+        List<Apple> weirdApples = FilteringApples.filterApples(inventory, a -> a.getWeight() < 80 || "brown".equals(a.getColor()));
+        System.out.println(weirdApples);
+    }
+}

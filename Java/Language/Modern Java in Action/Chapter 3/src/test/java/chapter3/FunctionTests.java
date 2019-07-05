4388da5721c5f1b801f79d25a19a8cd397182c4b
Roberto Foglia <fogliaroberto@hotmail.it>
2019-07-05 17:24:02 +0200

    Function examples

--- Java/Language/Modern Java in Action/Chapter 3/src/test/java/chapter3/FunctionTests.java
+++ Java/Language/Modern Java in Action/Chapter 3/src/test/java/chapter3/FunctionTests.java
@@ -1,5 +1,24 @@
 package chapter3;
 
-public class FunctionTests {
+import java.util.function.Function;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
+/** @@@ Function examples PAG 74 */
+@RunWith(MockitoJUnitRunner.class)
+public class FunctionTests {
+  @Test
+  public void functionTest() {
+    Function<Integer, Integer> sum = integer -> integer + 1;
+    Function<Integer, Integer> product = integer -> integer * 2;
+
+    // sum(product(2))
+    int result = sum.andThen(product).apply(2);
+    Assert.assertEquals(6, result);
+    // product(sum(2))
+    result = sum.compose(product).apply(2);
+    Assert.assertEquals(5, result);
+  }
 }

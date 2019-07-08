d5783b8e4e86093e6c92a2783ee9b70856d2e404
Roberto Foglia <fogliaroberto@hotmail.it>
2019-07-08 17:22:53 +0200

    Reducing

--- Java/Language/Modern Java in Action/Chapter 5/src/main/java/Reducing.java
+++ Java/Language/Modern Java in Action/Chapter 5/src/main/java/Reducing.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 
 import model.Dish;
 
+/**
+ * @@@ Reducing pag 111
+ */
 public class Reducing {
 
   public static void main(String... args) {
@@ -19,6 +22,8 @@ public class Reducing {
     int max = numbers.stream().reduce(0, (a, b) -> Integer.max(a, b));
     System.out.println(max);
 
+    numbers.stream().reduce(Integer::max).ifPresent(System.out::println);
+
     Optional<Integer> min = numbers.stream().reduce(Integer::min);
     min.ifPresent(System.out::println);
 
@@ -26,6 +31,11 @@ public class Reducing {
         .map(Dish::getCalories)
         .reduce(0, Integer::sum);
     System.out.println("Number of calories:" + calories);
+
+
+    // Quiz 5.3: Reducing
+    System.out.println(menu.stream().map(dish -> 1).reduce(0, Integer::sum));
+    System.out.println(menu.stream().count());
   }
 
 }

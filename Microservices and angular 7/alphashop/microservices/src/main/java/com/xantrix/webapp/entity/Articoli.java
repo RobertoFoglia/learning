1fd5e9913364139d20f6d226a8dc1e1fdadc13c3
Roberto Foglia <roberto.foglia.computer.scientist@gmail.com>
2019-10-13 10:42:21 +0200

    Creating entity classes and relations         Entity classes creation 			Lombok 			Hibernate annotations 		Relations amongs entity classes 			@JsonManagedReference 			@JsonBackReference 			@PrimaryKeyJoinColumn - primary and reference key

---
+++ Microservices and angular 7/alphashop/microservices/src/main/java/com/xantrix/webapp/entity/Articoli.java
@@ -0,0 +1,59 @@
+package com.xantrix.webapp.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "ARTICOLI")
+@Data
+public class Articoli implements Serializable {
+    private static final long serialVersionUID = 5367024664545455706L;
+
+    @Id
+    @Column(name = "CODART")
+    private String codArt;
+
+    @Column(name = "DESCRIZIONE")
+    private String descrizione;
+
+    @Column(name = "UM")
+    private String um;
+
+    @Column(name = "CODSTAT")
+    private String codStat;
+
+    @Column(name = "PZCART")
+    private Integer pzCart;
+
+    @Column(name = "PESONETTO")
+    private double pesoNetto;
+
+    @Column(name = "IDSTATOART")
+    private String idStatoArt;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "DATACREAZIONE")
+    private Date dataCreaz;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "articolo", orphanRemoval = true)
+    @JsonManagedReference
+    private Set<Barcode> barcode = new HashSet<>();
+
+    @OneToOne(mappedBy = "articolo", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private Ingredienti ingredienti;
+
+    @ManyToOne
+    @JoinColumn(name = "IDIVA", referencedColumnName = "idIva")
+    private Iva iva;
+
+    @ManyToOne
+    @JoinColumn(name = "IDFAMASS", referencedColumnName = "ID")
+    private FamAssort famAssort;
+}
---
+++ Microservices and angular 7/alphashop/microservices/src/main/java/com/xantrix/webapp/entity/Ingredienti.java
@@ -0,0 +1,26 @@
+package com.xantrix.webapp.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "INGREDIENTI")
+@Data
+public class Ingredienti implements Serializable {
+    private static final long serialVersionUID = -6597932485001138522L;
+
+    @Id
+    @Column(name = "CODART")
+    private String codArt;
+
+    @Column(name = "INFO")
+    private String info;
+
+    @OneToOne
+    @PrimaryKeyJoinColumn
+    @JsonBackReference
+    private Articoli articolo;
+}

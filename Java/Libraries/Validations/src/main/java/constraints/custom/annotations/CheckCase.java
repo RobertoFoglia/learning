1178b8a9e899d114d5eeb3d10d2762bb52157a58
Roberto Foglia <fogliaroberto@hotmail.it>
2020-05-12 13:20:05 +0200

    Simple custom validator

---
+++ Java/Libraries/Validations/src/main/java/constraints/custom/annotations/CheckCase.java
@@ -0,0 +1,53 @@
+package constraints.custom.annotations;
+
+import constraints.custom.CaseMode;
+import constraints.custom.CheckCaseValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({FIELD, METHOD, PARAMETER, ANNOTATION_TYPE, TYPE_USE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = CheckCaseValidator.class)
+@Documented
+@Repeatable(CheckCase.List.class)
+public @interface CheckCase {
+    /**
+     * an attribute message that returns the default key for
+     * creating error messages in case the constraint is violated
+     */
+    String message() default "{constraints.custom.annotations.CheckCase.message}";
+
+    /**
+     * an attribute groups that allows the specification of validation groups, to which this constraint belongs
+     * (see Chapter 5, Grouping constraints). This must default to an empty array of type Class<?>.
+     */
+    Class<?>[] groups() default {};
+
+    /**
+     * an attribute payload that can be used by clients of the Jakarta Bean Validation API to assign custom
+     * payload objects to a constraint. This attribute is not used by the API itself
+     */
+    // TODO to make an example, with dynamic payload (12.8. Dynamic payload as part of ConstraintViolation)
+    Class<? extends Payload>[] payload() default {};
+
+    CaseMode value();
+
+    @Target({FIELD, METHOD, PARAMETER, ANNOTATION_TYPE})
+    @Retention(RUNTIME)
+    @Documented
+    @interface List {
+        CheckCase[] value();
+    }
+}

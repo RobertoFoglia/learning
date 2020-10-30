b55a72601297e883225a10c26f4f303f50107684
Roberto Foglia <roberto.foglia.computer.scientist@gmail.com>
2020-10-30 13:32:12 +0100

    Documentation, OpenApi and Swagger

--- Quarkus/03 Microservices with Quarkus/code-with-quarkus/src/main/java/org/acme/AsynchronousRESTAPIController.java
+++ Quarkus/03 Microservices with Quarkus/code-with-quarkus/src/main/java/org/acme/AsynchronousRESTAPIController.java
@@ -3,6 +3,11 @@ package org.acme;
 import lombok.extern.slf4j.Slf4j;
 import org.acme.services.HelloWorldService;
 import org.eclipse.microprofile.context.ManagedExecutor;
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -18,6 +23,14 @@ import java.util.concurrent.CompletionStage;
 
 @Path("asyncAPIHello")
 @Slf4j
+/* @@@ Documentation, OpenAPI and Swagger */
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Asynchronous APIs",
+                version = "0.0.1",
+                description = "Quarkus examples of the asynchronous APIs"
+        )
+)
 public class AsynchronousRESTAPIController {
     @Inject
     @Named("default")
@@ -30,14 +43,27 @@ public class AsynchronousRESTAPIController {
     @Path("/async")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(
+            summary = "Hello world with async API.",
+            description = "description"
+    )
+    @APIResponses(
+            value = {
+                    @APIResponse(
+                            name = "Hello response.",
+                            responseCode = "200",
+                            description = "the system greetings you."
+                    )
+            }
+    )
     public void hello(@Suspended final AsyncResponse async) {
         managedExecutor.submit(() -> {
             log.info("Executing in a different thread");
 
             async.resume(
                     Response.ok(" Async -" + helloWorldService.greeting())
-                    .type(MediaType.APPLICATION_JSON_TYPE)
-                    .build());
+                            .type(MediaType.APPLICATION_JSON_TYPE)
+                            .build());
         });
         log.info("end of the rest method - Executing in a different thread");
     }
@@ -46,6 +72,19 @@ public class AsynchronousRESTAPIController {
     @Path("/rx")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(
+            summary = "Hello world with RX API",
+            description = "description"
+    )
+    @APIResponses(
+            value = {
+                    @APIResponse(
+                            name = "Hello response.",
+                            responseCode = "200",
+                            description = "the system greetings you."
+                    )
+            }
+    )
     public CompletionStage<Response> helloInRXWay() {
         final CompletionStage<Response>
                 completionStageResponse = new CompletableFuture<>();

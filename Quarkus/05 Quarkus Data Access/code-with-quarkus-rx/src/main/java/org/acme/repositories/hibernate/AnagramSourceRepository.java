714c7a5ebd52401487b775b35b24e01c20ed934b
Roberto Foglia <roberto.foglia.computer.scientist@gmail.com>
2020-11-03 13:24:27 +0100

    Cleaning of the project
    Signed-off-by: Roberto Foglia <roberto.foglia.computer.scientist@gmail.com>

--- Quarkus/05 Quarkus Data Access/code-with-quarkus-rx/src/main/java/org/acme/repositories/hibernate/AnagramSourceRepository.java
+++
@@ -1,7 +0,0 @@
-package org.acme.repositories.hibernate;
-
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import org.acme.entity.AnagramSource;
-
-public interface AnagramSourceRepository extends PanacheRepository<AnagramSource> {
-}
